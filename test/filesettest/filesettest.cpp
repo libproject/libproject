0847f6b01cc6aba6c66082a30915db7b0fece237
drewpts <andrey.poltavets@gmail.com>
2015-03-09 20:18:23 +0200

    Added some tests

--- test/filesettest/filesettest.cpp
+++ test/filesettest/filesettest.cpp
@@ -8,22 +8,73 @@ using LibprojManager::Interface::FileSetFactory;
 using LibprojManager::Interface::FileSetLoader;
 
 class FileSetTest : public ::testing::Test {
-    //FileSetTest() { }
-    //virtual ~FileSetTest() { }
 
-    virtual void SetUp() { }
-    virtual void TearDown() { }
+protected:
+    string projectNormalFile,
+           projectEmptyFile,
+           projectInconsistentFile,
+           projectBrokenFile;
+           FileSetLoader * loader = nullptr;
+
+public:
+    FileSetTest() { }
+    virtual ~FileSetTest() { }
+
+    virtual void SetUp() {
+        projectNormalFile = {"project_files/normal.libproject"};
+        projectEmptyFile  = {"project_files/empty.libproject"};
+        projectInconsistentFile = {"project_files/inconsistent.libproject"};
+        projectBrokenFile = {"project_files/empty.libproject"};
+    }
+    virtual void TearDown() {
+        delete loader;
+    }
 
 };
 
 
-TEST_F(FileSetTest, APIOpenNormalFile) {
-    const string projectFileName =
-            "project_files/normal.libproject";
-    FileSetLoader * loader =
-            FileSetFactory::createFileSet(projectFileName);
+/* TESTOPENFILE */
+TEST_F(FileSetTest, testOpenFile__NormalFile) {
+
+    if(loader)
+        delete loader;
+    loader = FileSetFactory::createFileSet(projectNormalFile);
     EXPECT_EQ(1, loader->open()) << "Test for open normal file";
 
+}
+
+TEST_F(FileSetTest, testOpenFile__EmptyFile) {
+
+    if(loader)
+        delete loader;
+    loader = FileSetFactory::createFileSet(projectEmptyFile);
+    EXPECT_EQ(1, loader->open()) << "Test for open empty file";
+
+}
+
+TEST_F(FileSetTest, testOpenFile__InconsistentFile) {
+
+    if(loader)
+        delete loader;
+    loader = FileSetFactory::createFileSet(projectInconsistentFile);
+    EXPECT_EQ(1, loader->open()) << "Test for open Inconsistent file";
+
+}
+
+TEST_F(FileSetTest, testOpenFile__BrokenFile) {
+
+    if(loader)
+        delete loader;
+    loader = FileSetFactory::createFileSet(projectBrokenFile);
+    EXPECT_EQ(1, loader->open()) << "Test for open Broken file";
+
+}
+
+TEST_F(FileSetTest, testGetFileNames__NormalFile) {
+    if(loader)
+        delete loader;
+    loader = FileSetFactory::createFileSet(projectNormalFile);
+    ASSERT_NE(0, loader->getFileNames().empty());
 }
 
 int main(int argc, char **argv) {
--- test/filesettest/filesettest.pro
+++ test/filesettest/filesettest.pro
@@ -27,3 +27,9 @@ DEPENDPATH += $$PWD/../../src/fileset
 
 QMAKE_CXXFLAGS += -std=c++11
 QMAKE_CXXFLAGS += -lpthread
+
+copydata.commands = $(COPY_DIR) $$PWD/project_files $$OUT_PWD
+first.depends = $(first) copydata
+export(first.depends)
+export(copydata.commands)
+QMAKE_EXTRA_TARGETS += first copydata
