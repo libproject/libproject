dd6367250617f97377fb8bd4ea8162474c2acb78
drewpts <andrey.poltavets@gmail.com>
2015-07-09 18:18:18 +0300

    [#46] Fixing review issue #94

--- include/libproject/libproject_error.h
+++ include/libproject/libproject_error.h
@@ -30,6 +30,27 @@ public:
         : runtime_error(message), errorType(type) { }
 
     ErrType getErrorType() const { return errorType; }
+
+    const std::string toString() const
+    {
+        switch (errorType)
+        {
+        case UnknownError:
+            return std::string("Unknown Error\nSee additional info through what() function");
+        case IncorrectSource:
+            return std::string("Encountered problem with source\nSee additional info through what() function");
+        case ProjectAlreadyLoaded:
+            return std::string("Loader have loaded project already");
+        case NotLoadedInterface:
+            return std::string("Loader haven\'t loaded project");
+        case AddBrokenSubproject:
+            return std::string("There was try to add broken subproject");
+        case LoadBrokenSubproject:
+            return std::string("There was try to load broken subproject");
+        case SubprojectsIncongruity:
+            return std::string("Some incongruity detected\nSee additional info through what() function");
+        }
+    }
 private:
     ErrType errorType;
 };
