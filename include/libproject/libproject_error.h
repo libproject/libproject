8a85e48a3d7f801310e72667d327b0c3d497e9a8
drewpts <andrey.poltavets@gmail.com>
2015-07-13 05:04:14 +0300

    [#46] fixing issues with exceptions design

--- include/libproject/libproject_error.h
+++ include/libproject/libproject_error.h
@@ -17,22 +17,86 @@ namespace Error {
 class FileSetRuntimeError : public std::runtime_error {
 public:
     enum ErrType {
-        UnknownError,
-        IncorrectSource,
-        AlreadyLoaded,
-        NotLoaded,
-        BrokenSubproject,
+        NotSpecified,
+        ProjectAlreadyLoaded,
+        ProjectNotLoaded,
+        AddBrokenSubproject,
+        LoadBrokenSubproject,
         SubprojectsIncongruity
     };
 
-    FileSetRuntimeError(const ErrType type, const std::string& message)
-        : runtime_error(message), errorType(type) { }
+    FileSetRuntimeError(ErrType type, const std::string& message = std::string())
+        : runtime_error(message) { }
 
     ErrType getErrorType() const { return errorType; }
-private:
+    virtual std::string errorDescription() const = 0;
+protected:
     ErrType errorType;
 };
+
+class UnknownRuntimeError : public FileSetRuntimeError {
+public:
+    UnknownRuntimeError(const std::string& message = std::string())
+        : FileSetRuntimeError(NotSpecified, message) { }
+
+    std::string errorDescription() const
+    {
+        return std::string("Unknown Error\nSee additional info through what() function");
+    }
+};
+
+class IncorrectSourceError : public FileSetRuntimeError {
+public:
+    IncorrectSourceError(const std::string& message = std::string())
+        : FileSetRuntimeError(NotSpecified, message) { }
+
+    std::string errorDescription() const
+    {
+        return std::string("Encountered problem with source\nSee additional info through what() function");
+    }
+};
+
+class LoaderStateError : public FileSetRuntimeError {
+public:
+    LoaderStateError(ErrType type, const std::string& message = std::string())
+        : FileSetRuntimeError(type, message) { }
+
+    std::string errorDescription() const
+    {
+        switch (errorType)
+        {
+        case ProjectAlreadyLoaded:
+            return std::string("Loader have loaded project already\nSee additional info through what() function");
+        case ProjectNotLoaded:
+            return std::string("Loader haven\'t loaded project\nSee additional info through what() function");
+        }
+    }
+};
+
+class SubprojectsError : public FileSetRuntimeError {
+public:
+    SubprojectsError(ErrType type, const std::string& message = std::string())
+        : FileSetRuntimeError(type, message) { }
+
+    std::string errorDescription() const
+    {
+        switch (errorType)
+        {
+        case AddBrokenSubproject:
+            return std::string("There was try to add broken subproject\nSee additional info through what() function");
+        case LoadBrokenSubproject:
+            return std::string("There was try to load broken subproject\nSee additional info through what() function");
+        case SubprojectsIncongruity:
+            return std::string("Some incongruity detected\nSee additional info through what() function");
+        }
+    }
+};
+
 
 } // namespace Error
 } // namespace Interface
 } // namespace LibprojManager
+
+// x в FileSetRuntimeError убрать второй аргумент
+// ? создать абстрактную функцию ErrDescr() в FileSetRuntimeError
+//породить классы от FileSetRuntimeError
