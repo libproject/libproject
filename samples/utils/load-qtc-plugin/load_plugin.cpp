7f8a1fe275a47d87303b0f18598f81b7dc211cf9
drewpts <andrey.poltavets@gmail.com>
2015-08-06 20:44:14 +0300

    fixed review issue

--- samples/utils/load-qtc-plugin/load_plugin.cpp
+++ samples/utils/load-qtc-plugin/load_plugin.cpp
@@ -1,6 +1,7 @@
 #include "extensionsystem/iplugin.h"
 #include "extensionsystem/extensionsystem_global.h"
 #include "extensionsystem/pluginmanager.h"
+#include "extensionsystem/pluginspec.h"
 #include "aggregation/aggregate.h"
 #include <QObject>
 #include <QStringList>
@@ -11,5 +12,11 @@ using namespace ExtensionSystem;
 int main()
 {
     PluginManager pluginManager;
+    QStringList pathToPlugin;
+    pathToPlugin.append(QString("../../../src/"));
+    PluginManager::setPluginPaths(pathToPlugin);
+    PluginManager::loadPlugins();
+    QList<PluginSpec *> specs = pluginManager.plugins();
+    qDebug() << specs.front()->name();
     return 0;
 }
