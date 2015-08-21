4b76cf3715794fdfa5efd4c688fd48e932c0995a
drewpts <andrey.poltavets@gmail.com>
2015-08-21 19:24:07 +0300

    #LPROJ-34 : target normalize_codestyle has been added

--- cmake/Modules/FindClangFormat.cmake
+++ cmake/Modules/FindClangFormat.cmake
@@ -4,9 +4,9 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(clang-format DEFAULT_MSG CLANGFORMAT_EXECUTABL
 # only visible in advanced view
 MARK_AS_ADVANCED(CLANGFORMAT_EXECUTABLE)
 
-if(NOT CLANGFORMAT_EXECUTABLE)
+if (NOT CLANGFORMAT_EXECUTABLE)
  set(CLANGFORMAT_FOUND FALSE)
- message(STATUS "clang-format not found! You can't perform codestyle normalizing by make target...")
-elseif()
+ message(WARNING "clang-format not found! You can't normalize codestyle by make target...")
+elseif ()
  set(CLANGFORMAT_FOUND TRUE)
-endif()
+endif ()
