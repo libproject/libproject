00557eb9141bc333e72e515625c7669288651d2d
drewpts <andrey.poltavets@gmail.com>
2015-08-21 09:03:22 +0300

    #LPROJ-32 : two cmake modules merged in one

--- cmake/Modules/FindQtC.cmake
+++ cmake/Modules/FindQtC.cmake
@@ -7,14 +7,15 @@
 # QTC_BLD_CORELIB - path to core library of QtCreator
 # QTC_BLD_AGGREGATIONLIB - Path to aggregation library of QtCreator
 # QTC_BLD_EXTSYSLIB - Path to extension system library of QtCreator
-#
+# QTC_SRC - path to QtCreator's sources
 # Usage sample:
-# find_package(QtC)
-# if (QTC_BLD_FOUND)
-#  do_some_stuff...
-# endif(QTC_BLD_FOUND)
-# target_link_libraries(<some_target> ${QTC_BLD_CORELIB})
+### find_package(QtC)
+### target_link_libraries(<some_target> ${QTC_BLD_CORELIB})
 #
+
+
+
+# Searching for QtC build
 set(PATH_TO_QTC_BUILD "" CACHE PATH "Defined by user path to QtCreator build")
 
 if (NOT "${PATH_TO_QTC_BUILD}" STREQUAL "")
@@ -54,10 +55,35 @@ if(QTC_BLD_FOUND)
   endif ()
 endif ()
 
+# Searching for QtC source
+set(PATH_TO_QTC_SOURCE "" CACHE PATH "Defined by user QtCreator's sources path")
+
+if (NOT "${PATH_TO_QTC_SOURCE}" STREQUAL "")
+  set(QTC_SRC ${PATH_TO_QTC_SOURCE} CACHE PATH "QtCreator's sources path")
+  set(QTC_SRC_FOUND TRUE)
+else ()
+  if (NOT "$ENV{QTC_SOURCE}" STREQUAL "")
+    set(QTC_SRC $ENV{QTC_SOURCE} CACHE PATH "QtCreator's sources path")
+    set(QTC_SRC_FOUND TRUE)
+  else ()
+    set(PATH_TO_QTC_SOURCE_ALONG_WITH_PROJECT "${CMAKE_SOURCE_DIR}/../qt-creator")
+    message(STATUS "QTC_SOURCE environment variable missing. QtCreator's sources will be searched at \"${CMAKE_SOURCE_DIR}/../qt-creator\"")
+    find_path(QTC_SRC_PROJECT_FILE qtcreator.pro HINTS ${PATH_TO_QTC_SOURCE_ALONG_WITH_PROJECT})
+    if (NOT QTC_SRC_PROJECT_FILE)
+      message(FATAL "QtCreator sources weren't found")
+      set(QTC_SRC_FOUND FALSE)
+    else ()
+      set(QTC_SRC ${PATH_TO_QTC_SOURCE_ALONG_WITH_PROJECT} CACHE PATH "QtCreator's sources path")
+      set(QTC_SRC_FOUND TRUE)
+    endif ()
+  endif ()
+endif ()
+
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(QtC DEFAULT_MSG
 QTC_BLD
 QTC_BLD_BINARY
 QTC_BLD_CORELIB
 QTC_BLD_EXTSYSLIB
+QTC_SRC
 )
